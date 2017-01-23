d22c1e94091b98d583bbb25c6b847c237108f5b0
FrenchYeti <georges.michel@protonmail.com>
2017-01-23 17:18:02 +0100

    Add hashCode, state, varname guessing

--- src/Tainted.php
+++ src/Tainted.php
@@ -7,14 +7,21 @@ namespace Seddkit;
  */
 abstract class Tainted
 {
-	protected $_varname = null;
     
-	/**
-	 * Enables/Disables the taint checking function
-	 * @var boolean		True enables the taint checking function. False disables it 
-	 */
-	public static $TaintChecking = true;
-	
+    protected static $nextID = 0;
+    
+    protected $_hashCode = null;
+    
+    protected $_state = null;
+    
+    protected $_varname = null;
+    
+    
+    protected function newHashCode()
+    {
+        $this->_hashCode = __CLASS__."::".self::$nextID++;
+    }
+    
 	/**
 	 * To indicate that the string is tainted
 	 * @var boolean		True means the string is tainted. False otherwise
@@ -30,6 +37,7 @@ abstract class Tainted
 	{
 		return $this->tainted;
 	}
+    
 	
 	/**
 	 * To decontaminate a "Tainted" object
@@ -51,6 +59,19 @@ abstract class Tainted
         return $this->_varname;
     }
     
+    /**
+    *
+    */
+    abstract public function newDerivation($method_str, $args_mix=null);
+    
+    
+    /**
+    *
+    */
+    public function printState($nl="\n",$space=" ")
+    {
+        return $this->_state->sprint($nl, $space);
+    }
     
     public function isMemberOf($parent_mix){
         foreach($parent_mix as $k=>$v){
@@ -62,6 +83,50 @@ abstract class Tainted
             
         }
         return false;
+    }
+    
+    public function getHashCode()
+    {
+        return $this->_hashCode;
+    }
+    
+    protected static $_counter  =0;
+    
+    /**
+    * Guess the varname if is not set :
+    * Start by scanning for $GLOBALS if $scope is NULL
+    */
+    public function guessVarName($scope=null, $prefix='$')
+    {
+        if(is_null($scope)) 
+            $scope = $GLOBALS;
+      
+        foreach($scope as $k=>$v){
+            
+            if($v instanceof Tainted){
+                if($this->isSelf($prefix.$k, $v)){
+                    $this->_varname = $prefix.$k;
+                    $this->_state->updateVarName($this->_varname);
+                    return true;
+                }
+            }else if(($k!=='GLOBALS') && is_array($v) && count($v)>0){  
+                if($this->guessVarName($v, $prefix.$k))
+                    return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    /**
+    *
+    */
+    public function isSelf($var_name,$var_value)
+    {
+        if($this->_hashCode == $var_value->_hashCode)
+            return true;
+        else
+            return false;
     }
 }
 
