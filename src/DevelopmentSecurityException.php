9038500f27484e9df7f31c13a72a00d568aec802
FrenchYeti <georges.michel@protonmail.com>
2017-01-23 17:20:56 +0100

    Add state to the message

--- src/DevelopmentSecurityException.php
+++ src/DevelopmentSecurityException.php
@@ -20,11 +20,19 @@ class DevelopmentSecurityException extends \Exception
     
     protected $message = null;
     
-    public function __construct($subject_mix, $rulename_str, \Exception $previous = null)
+    public function __construct($subject_mix, $rulename_str, $context=null, \Exception $previous = null)
     {
         parent::__construct("", 0, $previous);
         
+        // try to detect concatenation, cast, echo or print
+        if($context === Seddkit::STRING_CONTEXT){
+            
+        }
+        
         $trace = $this->getTrace()[count($this->getTrace())-1];
+        
+        var_dump($trace);
+        
         $rule = Seddkit::RULE($rulename_str);
         
         if($subject_mix instanceof Tainted)
@@ -33,13 +41,17 @@ class DevelopmentSecurityException extends \Exception
             $subject = var_export($subject_mix, true);
         
 
-        $this->message = (Seddkit::isBypassed($rulename_str)? " [*] BYPASSED : " : "[!] ");
+        $this->message = (Seddkit::isBypassed($rulename_str)? "<pre>[*] BYPASSED : " : "<pre>[!] ");
         $this->message .= sprintf('<b>'.$rule['title'].'</b>',htmlentities($subject));
         $this->message .= "<br><i>File : ".htmlentities(isset($trace['file'])?$trace['file']:"[PHP Internals]").
             " , Line : ".((isset($trace['line'])?(int)$trace['line']:0)).
             ", ".(($trace['function']=='__toString')?'':"Function : {$trace['function']}()")."</i><br>";
         $this->message .= sprintf('%s<br>En savoir plus : %s<br>',htmlentities($rule['msg']),$rule['link']);
         
+        if(Seddkit::CONFIG('PRINT_STATE') && ($subject_mix instanceof Tainted))
+           $this->message .= $subject_mix->printState("<br>","&nbsp;");
+        
+        $this->message .= '</pre>';
         if(Seddkit::CONFIG('DISPLAY_ERR')){
             echo $this->message;
         }
